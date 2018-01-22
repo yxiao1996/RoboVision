60ec6fbea17ddfd04ac3554e6958bde9e4146797
yxiao1996 <xiaoyu960522@sina.com>
2018-01-22 10:54:16 +0800

    add preproc file

---
+++ modules/object_detector/preproc.py
@@ -0,0 +1,28 @@
+import cv2
+import glob
+
+source_dir = '/home/yxiao1996/data/balls/new/'
+target_dir = '/home/yxiao1996/data/balls/pos/'
+img_format = '.jpeg'
+
+def getImgNum():
+    blob = glob.glob(target_dir+'*'+img_format)
+    num_img = len(blob)
+
+    return num_img
+
+def procImage(img_dir, save_dir, img_num, size=(960,540)):
+    print "processing images..."
+    img_iter = glob.iglob(img_dir+'*')
+    img_names = []
+    imgs = []
+    for i, img in enumerate(img_iter):
+        img_names.append(img)
+        image = cv2.imread(img)
+        img_resize = cv2.resize(image, size)
+        save_name = save_dir + str(i+img_num) + img_format
+        cv2.imwrite(save_name, img_resize)
+
+if __name__ == '__main__':
+    img_num = getImgNum()
+    procImage(source_dir, target_dir, img_num)
